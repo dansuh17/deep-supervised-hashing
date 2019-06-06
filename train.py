63ba954841ba111d212f721ae32e807dc9af67f3
Dan Suh <kaehops@gmail.com>
2019-06-07 01:54:53 +0900

    Generate hash codes and log the text

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ Train DSH on MNIST
 4. Test image retrieval from Hamming space
 5. do a t-SNE on Hamming space
 """
+from collections import defaultdict
 import random
 import torch
 from torch import nn
@@ -16,6 +17,7 @@ from torchvision.datasets.mnist import MNIST
 from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
+import numpy as np
 from model import LiuDSH
 
 # hyper-parameters
@@ -183,12 +185,34 @@ class Trainer:
                 torch.cat(embeddings).cpu().numpy(),
                 global_step=self.global_step)
 
-            # draw embeddings for a single batch
+            # draw embeddings for a single batch - very nice for visualizing clusters
             self.writer.add_embedding(
                 torch.cat(hash_embeddings),
                 metadata=torch.cat(test_targets),
                 label_img=torch.cat(test_imgs),
                 global_step=self.global_step)
+
+            # TODO: print text as hexadecimal strings
+            hash_vals = torch.cat(hash_embeddings).numpy().astype(int)
+            hash_vals = np.packbits(hash_vals, axis=-1)  # to uint8
+            targets = torch.cat(test_targets).numpy().astype(int)
+
+            hashdict = defaultdict(list)
+            for target_class, hash_value in zip(targets, hash_vals):
+                print(target_class)  # TODO
+                print(hash_value)  # TODO
+                hashdict[target_class].append(f'{hash_value[0]:#04x}')  # ex) 15 -> 0x0f
+
+            result_texts = []
+            for target_class in sorted(hashdict.keys()):
+                for hashval in hashdict[target_class]:
+                    result_texts.append(f'class: {target_class:02d} - {hashval}')
+                    self.writer.add_text(
+                        f'e{self.global_epoch}_hashvals/{target_class:02d}',
+                        hashval, global_step=self.global_step)
+
+            result_text = '\n'.join(result_texts)
+            print(result_text)  # TODO
 
             self.global_epoch += 1
 
